f1221f3ff654419451846b0d70b4422368ec6a40
gv1_ <44043159+march0s1as@users.noreply.github.com>
2021-08-23 21:17:16 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# cores
+normal=$'\e[0m'  
+C=$(printf '\033')                                                 
+green="${C}[1;32m"
+yellow="${C}[1;33m"
+RED="${C}[1;31m"
+# fim das cores
+
+instalacao(){
+	echo "${green}[OK] ${normal}iniciando a instalação."
+	echo "${yellow}[!!] ${normal}procurando path do go."
+
+	if [ -x "$(command -v go)" ]; then
+		echo "${green}[OK] ${normal}sucesso ao encontrar."
+		go mod init pink.go
+		go mod tidy	
+		go build pink.go
+		echo "${green}[OK] ${normal}feito! para rodar a ferramenta, digite ${yellow}go run pink.go${normal} ou apenas ${yellow}./pink"
+
+	else
+		echo "${RED} favor instalar golang em seu sistema."
+	fi
+}
+
+instalacao
